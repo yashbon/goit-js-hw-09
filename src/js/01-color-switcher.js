8d7826207db16a5fbb3b73a9eaf8a144f179d4c6
yashbon <104573777+yashbon@users.noreply.github.com>
2023-04-13 03:28:49 +0300

    Completed Task - 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const backgroundBody = document.querySelector('body');
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+// let idInterval;
 
+buttonStop.disabled = true;
+
+buttonStart.addEventListener('click', onButtonStart);
+// buttonStop.addEventListener('click', onButtonStop);
+
+function onButtonStart() {
+
+    const idInterval = setInterval(() => { backgroundBody.style.backgroundColor = getRandomHexColor() }, 1000);
+    buttonStart.disabled = true;
+    buttonStop.disabled = false;
+
+    buttonStop.addEventListener('click', onButtonStop);
+
+    function onButtonStop() {
+        buttonStart.disabled = false;
+        buttonStop.disabled = true;
+        clearInterval(idInterval);
+    }
+}
+
+// function onButtonStop() {
+//     buttonStart.disabled = false;
+//     buttonStop.disabled = true;
+//     clearInterval(id);
+// }
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
